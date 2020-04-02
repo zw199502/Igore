ffcee4498751a68f61e746ec590eebf049f7e08a
FahadRazaSJTU <fahadkhan441@hotmail.com>
2020-04-02 19:12:13 +0900

    JointState Publisher added

--- src/example_01_lookup_node.cpp
+++ src/example_01_lookup_node.cpp
@@ -7,12 +7,17 @@
 #include "hebi_cpp_api/group.hpp"
 #include "hebi_cpp_api/group_command.hpp"
 #include "hebi_cpp_api/group_feedback.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include "geometry_msgs/Quaternion.h"
+#include "tf/transform_datatypes.h"
+#include "sensor_msgs/JointState.h"
 #include <Eigen/Dense>
 #include <chrono>
 #include <thread>
 #include <iostream>
 #include <string>
 #include <cmath> // provides M_PI
+#include <typeinfo>
 
 using namespace hebiros;
 
@@ -23,6 +28,14 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "example_01_lookup_node");
   ros::NodeHandle n;
   ros::Rate loop_rate(400);
+
+  tf::Quaternion tf_quat;
+  geometry_msgs::Quaternion geom_quat;
+  double roll, pitch, yaw;
+
+  sensor_msgs::JointState igor_joint_states;
+  ros::Publisher  joint_pub;
+  joint_pub = n.advertise<sensor_msgs::JointState>( "/joint_states", 1 );
 
   std::string group_name = "my_group";
   // Create a hebi Lookup Object
@@ -67,18 +80,18 @@ int main(int argc, char **argv) {
   
   
   Eigen::Vector2d w;
-  w(0) = 0*(2.0 * M_PI);
+  w(0) = (2.0 * M_PI);
   w(1) = 0*(4.0 * M_PI);
   Eigen::Vector2d w_t(0,0);
   double t = 0.0;
   double dt = 0.01; // 10 ms
   while (ros::ok()) 
   {
-    w_t = 0* w * t;
-    std::cout << "w_t = " << w_t << std::endl;
+    w_t = w * t;
     groupCommand.setPosition(w_t.array().cos());
     groupCommand.setVelocity(0.5*w_t.array().sin());
-    groupCommand[1].actuator().position().set(0.78); // Position command to Elbow only
+    groupCommand[1].actuator().position().set(0); // Position command to Elbow only
+    //groupCommand[0].actuator().position().set(0.78); // Position command to Shoulder only
     group->sendCommand(groupCommand);
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
     t += dt;
@@ -96,6 +109,32 @@ int main(int argc, char **argv) {
     else{
       std::cout << "No shoulder position feedback!" << std::endl;
     }
+
+    const auto& Elbow_orientation = feedback[1].imu().orientation();
+    const auto orient_quat = Elbow_orientation.get();
+    std::cout << "orient_quat type:  " << typeid(orient_quat).name() << std::endl;
+    
+    geom_quat.x = orient_quat.getX();
+    geom_quat.y = orient_quat.getY();
+    geom_quat.z = orient_quat.getZ();
+    geom_quat.w = orient_quat.getW();
+    
+    tf::quaternionMsgToTF(geom_quat, tf_quat);
+    tf_quat.normalize(); // normalize the quaternion in case it is not normalized
+    
+    //the tf::Quaternion has a method to acess roll pitch and yaw
+    tf::Matrix3x3(tf_quat).getRPY(roll, pitch, yaw);
+
+
+    if (Elbow_orientation.has()){
+      std::cout << "Elbow Roll: "<< roll  << std::endl;
+      std::cout << "Elbow Pitch: "<< pitch  << std::endl;
+      std::cout << "Elbow Yaw: "<< yaw  << std::endl;
+    }
+    else{
+      std::cout << "No elbow orientation feedback!" << std::endl;
+    }
+    
     // Retrieve velocities:
     Eigen::VectorXd velocities = feedback.getVelocity();
     std::cout << "Velocity feedback: " << std::endl << velocities << std::endl;
@@ -108,54 +147,47 @@ int main(int argc, char **argv) {
     Eigen::MatrixX3d gyros = feedback.getGyro(); // Matrix of dynamic rows and 3 colums
     std::cout << "Gyro feedback: " << std::endl << gyros << std::endl;
 
+    const auto& Elbow_gyro = feedback[1].imu().gyro(); // Matrix of dynamic rows and 3 colums
+    std::cout << "Elbow Gyro_x: " << std::endl << Elbow_gyro.get().getX() << std::endl;
+
+    /** ROS JointStates publisher **/
+    igor_joint_states.header.stamp = ros::Time::now();
+    igor_joint_states.name.push_back("L_hfe_joint");
+    igor_joint_states.name.push_back("L_kfe_joint");
+    igor_joint_states.name.push_back("L_wheel_joint");
+    igor_joint_states.name.push_back("R_hfe_joint");
+    igor_joint_states.name.push_back("R_kfe_joint");
+    igor_joint_states.name.push_back("R_wheel_joint");
+    // Joint positions rads.
+    igor_joint_states.position.push_back(positions(0));
+    igor_joint_states.position.push_back(positions(1));
+    igor_joint_states.position.push_back(0);
+    igor_joint_states.position.push_back(0);
+    igor_joint_states.position.push_back(0);
+    igor_joint_states.position.push_back(0);
+    // Joint velocities rad/s
+    igor_joint_states.velocity.push_back(velocities(0));
+    igor_joint_states.velocity.push_back(velocities(1));
+    igor_joint_states.velocity.push_back(0);
+    igor_joint_states.velocity.push_back(0);
+    igor_joint_states.velocity.push_back(0);
+    igor_joint_states.velocity.push_back(0);
+    // Joint efforts N.m 
+    igor_joint_states.effort.push_back(0);
+    igor_joint_states.effort.push_back(0);
+    igor_joint_states.effort.push_back(0);
+    igor_joint_states.effort.push_back(0);
+    igor_joint_states.effort.push_back(0);
+    igor_joint_states.effort.push_back(0);
+    // Publishing on the topic
+    joint_pub.publish(igor_joint_states);
+    //Clearing the arrays
+    igor_joint_states.name.clear();
+    igor_joint_states.position.clear();
+    igor_joint_states.velocity.clear();
+    igor_joint_states.effort.clear();
   }
   /**##############################**/
-  //Create a client which uses the service to see the entry list of modules
-  ros::ServiceClient entry_list_client = n.serviceClient<EntryListSrv>("/hebiros/entry_list");
-
-  ROS_INFO("entry_list_client created");
-
-  //Create a client which uses the service to create a group
-  ros::ServiceClient add_group_client = n.serviceClient<AddGroupFromNamesSrv>("/hebiros/add_group_from_names");
-
-  ROS_INFO("Group created");
-
-  //Create a client which uses the service to find the size of a group
-  ros::ServiceClient size_client = n.serviceClient<SizeSrv>("/hebiros/"+group_name+"/size");
-
-  ROS_INFO("Size of Group created");
-
-  EntryListSrv entry_list_srv;
-  AddGroupFromNamesSrv add_group_srv;
-  SizeSrv size_srv;
-
-  //Call the entry_list service, displaying each module on the network
-  //entry_list_srv.response.entry_list will now be populated with those modules
-  entry_list_client.call(entry_list_srv);
-  ROS_INFO("entry_list service called");
-
-  //Construct a group using 2 known modules
-  add_group_srv.request.group_name = group_name;
-  add_group_srv.request.names = {"Shoulder", "Elbow"};
-  add_group_srv.request.families = {"X5-1"};
-  ROS_INFO("2 module group constructed");
-  //Call the add_group_from_urdf service to create a group until it succeeds
-  //Specific topics and services will now be available under this group's namespace
- // while(!add_group_client.call(add_group_srv)) {
- //   ROS_INFO("In while loop");
- // }
-
-  //Call the size service for the newly created group
-  size_client.call(size_srv);
-  ROS_INFO("%s has been created and has size %d", group_name.c_str(), size_srv.response.size);
-
-  //Spin
-  //The group will exist until the node is shutdown
-  while(ros::ok()) {
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-
   return 0;
 }
 
